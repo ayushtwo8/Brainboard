9039eb934e38b8b7256f0337589d474d1d682407
Ayush Tiwari <ayushh8866@gmail.com>
2025-06-23 08:55:34 +0530

    changes

--- server/dist/schemas/userSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userSchema = void 0;
-const zod_1 = require("zod");
-exports.userSchema = zod_1.z.object({
-    name: zod_1.z.string().min(2).optional(),
-    email: zod_1.z.string().email(),
-    password: zod_1.z.string().min(5, "Password should contain min 6 letters")
-});
